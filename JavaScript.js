fea0154046da8b965774afd3049215ffd9fde8f3
Saksham-Shukla-05 <get467997@gmail.com>
2024-08-22 23:34:58 +0530

    Changed the Styling Little Bit

---
+++ JavaScript.js
@@ -0,0 +1,114 @@
+let flag = 0;
+
+function getans() {
+  if (flag == 0) {
+    let answer = document.getElementById("faq-ans");
+    answer.style.display = "block";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn");
+    butn.style.rotate = "90deg";
+    flag = 1;
+  } else {
+    let answer = document.getElementById("faq-ans");
+    answer.style.display = "none";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn");
+    butn.style.rotate = "44deg";
+    flag = 0;
+  }
+}
+
+function getans1() {
+  if (flag == 0) {
+    let answer = document.getElementById("faq-ans1");
+    answer.style.display = "block";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn1");
+    butn.style.rotate = "90deg";
+
+    flag = 1;
+  } else {
+    let answer = document.getElementById("faq-ans1");
+    answer.style.display = "none";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn1");
+    butn.style.rotate = "44deg";
+    flag = 0;
+  }
+}
+
+function getans2() {
+  if (flag == 0) {
+    let answer = document.getElementById("faq-ans2");
+    answer.style.display = "block";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn2");
+    butn.style.rotate = "90deg";
+
+    flag = 1;
+  } else {
+    let answer = document.getElementById("faq-ans2");
+    answer.style.display = "none";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn2");
+    butn.style.rotate = "44deg";
+    flag = 0;
+  }
+}
+
+function getans3() {
+  if (flag == 0) {
+    let answer = document.getElementById("faq-ans3");
+    answer.style.display = "block";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn3");
+    butn.style.rotate = "90deg";
+
+    flag = 1;
+  } else {
+    let answer = document.getElementById("faq-ans3");
+    answer.style.display = "none";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn3");
+    butn.style.rotate = "44deg";
+    flag = 0;
+  }
+}
+
+function getans4() {
+  if (flag == 0) {
+    let answer = document.getElementById("faq-ans4");
+    answer.style.display = "block";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn4");
+    butn.style.rotate = "90deg";
+
+    flag = 1;
+  } else {
+    let answer = document.getElementById("faq-ans4");
+    answer.style.display = "none";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn4");
+    butn.style.rotate = "44deg";
+    flag = 0;
+  }
+}
+
+function getans5() {
+  if (flag == 0) {
+    let answer = document.getElementById("faq-ans5");
+    answer.style.display = "block";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn5");
+    butn.style.rotate = "90deg";
+
+    flag = 1;
+  } else {
+    let answer = document.getElementById("faq-ans5");
+    answer.style.display = "none";
+    answer.style.transition = "all 2s";
+    let butn = document.getElementById("btn5");
+    butn.style.rotate = "44deg";
+    flag = 0;
+  }
+}
